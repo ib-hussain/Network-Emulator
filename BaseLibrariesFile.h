888dd38f9073d215a76eb15247e906c7655789a6
ib-hussain <ibrahimbeaconarion@gmail.com>
2025-05-07 18:24:16 +0500

    Made a base libraries file to include all the base libraries and also making basic structures that we will need throughout the project

---
+++ BaseLibrariesFile.h
@@ -0,0 +1,18 @@
+#ifndef BASELIBRARIESFILE_H
+#define BASELIBRARIESFILE_H
+
+#include <iostream>
+// any libraries that need to be added to the project should be included here and they will be included in all other files
+using namespace std;
+#define NULLint 0
+#define FULLint -32767
+#define NULLchar '\0'
+#define NULLstring ""
+#define NULLbool NULL
+#define NULLdouble 0.0000
+#define NULLfloat 0.0000
+#define NULLpointer nullptr
+// #define
+// make macro for reference to pointer type assignement
+
+#endif
--- Graph.h
+++ Graph.h
@@ -1,17 +1,9 @@
 #ifndef GRAPH_H
 #define GRAPH_H
+#include "BaseLibrariesFile.h"
 
-#include <iostream>
-using namespace std;
-#define NULLint 0
-#define FULLint -32767
-#define NULLchar '\0'
-#define NULLstring ""
-#define NULLbool NULL
-#define NULLdouble 0.0000
-#define NULLfloat 0.0000
-#define NULLpointer nullptr
-// #define
-// make macro for reference to pointer type assignement
+
+
+
 
 #endif
