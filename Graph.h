6c60695f3acfba26b7a09707a9df4f57599eb18b
ib-hussain <ibrahimbeaconarion@gmail.com>
2025-05-07 18:58:17 +0500

    Making the Graph class a template class as the base for the whole project

--- Graph.h
+++ Graph.h
@@ -1,9 +1,52 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 #include "BaseLibrariesFile.h"
+template <class dtype1 = int>//configure isf this type is okay for this project
+struct GraphNode{
+    dtype data;// this can be any datatype we choose to use in the graph
+    GPtrs pointers;
+    // i am making 1 graph node and making a linked list in it that holds the pointers to the other nodes in the graph
+    // this is a singly linked list of pointers to the other nodes in the graph
+};
+struct GPtrsNode{
+    int weight;
+    GraphNode* connection;
+    GPtrsNode(): weight(NULLint), connection(NULLpointer),  nextnode(NULLpointer) {}
+    GPtrsNode(int weighti, GraphNode* nexti): weight(weighti), connection(nexti) , nextnode(NULLpointer) {}
+    
+    friend class GPtrs;
+    int& operator*(){
+        return weight;
+    }
+    GraphNode* operator>>(int i=NULLint){
+        return connection;
+    }
+private:
+    GPtrsNode* nextnode;
+};
+struct GPtrs{
+private:
+    GPtrsNode* tail;
+    int nodes;
+    bool insert(GraphNode* newNode, int weighti){
+        if(!tail){
+            tail = new GPtrsNode(weighti, newNode);//fix this constructor call here
+            tail->nextnode = tail;
+            nodes++;
+            return true;
+        }
+        else{
 
-
-
+        }
+        return false;
+    }
+    bool remove
+public:
+    GPtrs(): head(NULLpointer), tail(NULLpointer), nodes(-1) {}
+    GPtrsNode& operator[](int i){
+        
+    }
+};
 
 
 #endif
---
+++ SinglyLinkedList.h
@@ -0,0 +1,7 @@
+#ifndef SINGLYLINKEDLIST_H
+#define SINGLYLINKEDLIST_H
+#include "BaseLibrariesFile.h"
+
+
+
+#endif
