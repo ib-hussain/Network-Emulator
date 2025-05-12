3d2eda97214c010d5ae87f33130c11090f32684f
ib-hussain <ibrahimbeaconarion@gmail.com>
2025-05-12 17:36:06 +0500

    fcgvbhjnkm,

--- network_emulator.cpp
+++ network_emulator.cpp
@@ -6,6 +6,7 @@ int main(){
 }
 void start_engine(){
     //CLI commands
+    // take all the data in the fucking commas as input but do parse the whole line and the way i have shown, extract data from it
     // send msg "FILE_NAME.txt"                         //1 type // it will give file name
     // send msgs "FILE_NAME.txt"                        //1 type // it will give file name
     // change RT //4 types:
@@ -15,14 +16,14 @@ void start_engine(){
         // change RT "ROUTER_NAME" remove "FILE_NAME.csv"                   // gives file name & router name
     // print path // 3 types:
         //  has a " to " in between
-            // print path "MACHINE_NAME" to "MACHINE_NAME" // gives a name of machine and another machine
-            // print path "MACHINE_NAME" to "*"            // gives a name of machine and any other machine
-            // print path "*" to "MACHINE_NAME"            // gives a name of machine and any other machine
+            // print path "MACHINE_NAME" to "MACHINE_NAME"                  // gives a name of machine and another machine
+            // print path "MACHINE_NAME" to "*"                             // gives a name of machine and any other machine
+            // print path "*" to "MACHINE_NAME"                             // gives a name of machine and any other machine
     // change egde // implement this in graph.h
         // 4 types:
-            // change egde "FILE_NAME.csv"                          // gives file name
-            // change egde "ROUTER_NAME","ROUTER_NAME","WEIGHT"     // gives a name of router and router and weight
-            // change egde "MACHINE_NAME","ROUTER_NAME","WEIGHT"    // gives a name of router and router and weight
-            // change egde "ROUTER_NAME","MACHINE_NAME","WEIGHT"    // gives a name of router and router and weight
+            // change egde "FILE_NAME.csv"                                  // gives file name
+            // change egde "ROUTER_NAME","ROUTER_NAME","WEIGHT"             // gives a name of router and router and weight
+            // change egde "MACHINE_NAME","ROUTER_NAME","WEIGHT"            // gives a name of router and router and weight
+            // change egde "ROUTER_NAME","MACHINE_NAME","WEIGHT"            // gives a name of router and router and weight
     //add a menu in this whole function
 }
