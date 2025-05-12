ae12521b29f6fbc51228b43936417ca234b060ec
ib-hussain <ibrahimbeaconarion@gmail.com>
2025-05-12 17:33:30 +0500

    made the menu fucntion ready to be made

--- network_emulator.cpp
+++ network_emulator.cpp
@@ -6,10 +6,23 @@ int main(){
 }
 void start_engine(){
     //CLI commands
-    // send msg //1 type
-    // send msgs //1 type
-    // change RT //4 types
-    // print path // 3 types has a " to " in between
-    // change egde // 3 types // implement this in graph.h
+    // send msg "FILE_NAME.txt"                         //1 type // it will give file name
+    // send msgs "FILE_NAME.txt"                        //1 type // it will give file name
+    // change RT //4 types:
+        // change RT "ROUTER_NAME" add "MACHINE_NAME","ROUTER_NAME"          // gives a name of machine and router & router name
+        // change RT "ROUTER_NAME" remove "MACHINE_NAME","ROUTER_NAME"       // gives a name of machine and router & router name
+        // change RT "ROUTER_NAME" add "FILE_NAME.csv"                      // gives file name & router name
+        // change RT "ROUTER_NAME" remove "FILE_NAME.csv"                   // gives file name & router name
+    // print path // 3 types:
+        //  has a " to " in between
+            // print path "MACHINE_NAME" to "MACHINE_NAME" // gives a name of machine and another machine
+            // print path "MACHINE_NAME" to "*"            // gives a name of machine and any other machine
+            // print path "*" to "MACHINE_NAME"            // gives a name of machine and any other machine
+    // change egde // implement this in graph.h
+        // 4 types:
+            // change egde "FILE_NAME.csv"                          // gives file name
+            // change egde "ROUTER_NAME","ROUTER_NAME","WEIGHT"     // gives a name of router and router and weight
+            // change egde "MACHINE_NAME","ROUTER_NAME","WEIGHT"    // gives a name of router and router and weight
+            // change egde "ROUTER_NAME","MACHINE_NAME","WEIGHT"    // gives a name of router and router and weight
     //add a menu in this whole function
 }
